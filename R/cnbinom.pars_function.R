977fd2de136ef8c6c6158db71b838ea7511853c9
Samantha Duchscherer <sam.duchscherer@gmail.com>
2018-09-24 18:00:03 +0000

    version 1.0.3

--- R/cnbinom.pars_function.R
+++ R/cnbinom.pars_function.R
@@ -1,18 +1,25 @@
 library(stringr)
-source("R/format_data_for_functions.R")
 
-# estimated_lambda is a function that outputs lambda from univariate censored table
+# cnbinom.pars is a function that outputs mu and r from univariate censored table input 
+# assume negative binomial 
 
-estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
+cnbinom.pars<-function (censoredtable){
   
-  ## functions that help format data first
-  #The findtypeofcensoring_univariatetable Function list the type of censoring in a provided censored frequency table
-  #Type_of_Censoring (L1 (<), L2 (<=), I (-), G1 (>), G2 (>= or +), or U (no censoring)), and Censor_Number (category value corresponding to censoring value)
   
-  findtypeofcensoring_univariatetable<-function (contingencytable){
+  # make probabilities if not already
+  censoredtable<-data.frame(censoredtable)
+  names(censoredtable)<-c("cateogry", "value")
+  censoredtable$value<-as.numeric(as.character(censoredtable$value))
+  
+  if (sum(as.numeric(censoredtable$value)) !=1){
+    censoredtable$value<-censoredtable$value/sum(censoredtable$value)
+  }
+  
+#The findtypeofcensoring_univariatetable function regonizes the symbols and divides a frequency table up into the 6 censored cases
+findtypeofcensoring_univariatetable<-function (univariatefreqtable){
     
     #making a dataframe from the category case (column one)
-    cases<-data.frame(contingencytable)
+    cases<-data.frame(univariatefreqtable)
     cases<-cases[1]
     #relabeled column with censoring as Censoring_Symbol
     names(cases) <- c("Censoring_Symbol")
@@ -30,6 +37,7 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
     
     # remove numbers from censoring_symbol column
     cases$Censoring_Symbol<-gsub('[0-9]+', '', cases$Censoring_Symbol)
+    cases$Censoring_Symbol<-gsub('\\.', '', cases$Censoring_Symbol)
     
     #named the symbols allowed in model
     L1 <- c("<")
@@ -44,7 +52,7 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
     G2a<-c("[+]")
     G2b<-c("GE")
     
-    # remove any spaces found in symbol column 
+    # remove any spaces found in symbol column
     cases$Censoring_Symbol<-gsub('\\s+', '', cases$Censoring_Symbol)
     
     #look to see if these symbols are in the provided table
@@ -94,7 +102,7 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
     #listing the no censoring cases as "U"
     cases$Type_of_Censoring[cases$Type_of_Censoring==""]<-"U"
     
-    #put in errors to let users know that there can't be duplicates of Greater than or Less than 
+    #put in errors to let users know that there can't be duplicates of Greater than or Less than
     if(length(grep("G1", cases$Type_of_Censoring))>1) stop ('Censored table can only have 1 greater than (> or G) category')
     if(length(grep("G2", cases$Type_of_Censoring))>1) stop ('Censored table can only have 1 greater than or equal to (>= or GE) category')
     if(length(grep("L2", cases$Type_of_Censoring))>1) stop ('Censored table can only have 1 less than or equal to (<= or LE) category')
@@ -103,30 +111,20 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
     if(length(grep("L1", cases$Type_of_Censoring))==1 && length(grep("L2", cases$Type_of_Censoring))==1) stop ('Censored table can not  have both a less than (< or L) category and a less than or equal to category (<= or LE)')
     if(length(grep("G1", cases$Type_of_Censoring))==1 && length(grep("G2", cases$Type_of_Censoring))==1) stop ('Censored table can not have both a greater than (< or G) category and a greater than or equal to category (<= or GE)')
     
-    
-    
     return (cases)} #end findtypeofcensoring_univariatetable function
   
   #####
   #####
   
   #The fixdata_univariatecase function regonizes the symbols and divides a frequency table up into the 6 censored cases
-  #This is a formatting data function 
+  #This is a formatting data function
   #This uses the findtypeofcensoring_univariatetable function
-  #Requirement of lower and upper bounds
-  #The lower bound provided will be used in calculating a right shift and upper bound for truncation
-  
-  
-  fixdata_univariatecase<-function(contingencytable, upperbound, quasipoisson_phi) {
+
+  fixdata_univariatecase<-function(univariatefreqtable) {
     
-    # conduct findtypeofcensoring_univariatetable function on data 
-    cases<-findtypeofcensoring_univariatetable(data.frame(contingencytable))
-    
-    # stop if upperbound is smaller than largest number in table
-    largestnumintable<-max(as.numeric(unlist(regmatches(cases$Censor_Number, gregexpr("[[:digit:]]+", cases$Censor_Number)))))
-    if(largestnumintable>upperbound) stop ('Upperbound needs to be larger than or equal to its largest corresponding category number')
-    
-    
+    # conduct findtypeofcensoring_univariatetable function on data
+    cases<-findtypeofcensoring_univariatetable(data.frame(univariatefreqtable))
+   
     #gives a row number for each of the 6 censoring types
     lo1<-as.numeric(which(cases$Type_of_Censoring=='L1'))
     lo2<-as.numeric(which(cases$Type_of_Censoring=='L2'))
@@ -138,12 +136,12 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
     #finding what number corresponds to the freqency value based off type of censoring
     #put as.numeric and as.character to help with format
     #R was changing decimals to whole numbers without putting as.numeric and as.character
-    countl1<-as.numeric(as.character(contingencytable[lo1,2]))
-    countl2<-as.numeric(as.character(contingencytable[lo2,2]))
-    counti<-as.numeric(as.character(contingencytable[inte,2]))
-    countg1<-as.numeric(as.character(contingencytable[gre1,2]))
-    countg2<-as.numeric(as.character(contingencytable[gre2,2]))
-    counte<-as.numeric(as.character(contingencytable[ex,2]))
+    countl1<-as.numeric(as.character(univariatefreqtable[lo1,2]))
+    countl2<-as.numeric(as.character(univariatefreqtable[lo2,2]))
+    counti<-as.numeric(as.character(univariatefreqtable[inte,2]))
+    countg1<-as.numeric(as.character(univariatefreqtable[gre1,2]))
+    countg2<-as.numeric(as.character(univariatefreqtable[gre2,2]))
+    counte<-as.numeric(as.character(univariatefreqtable[ex,2]))
     
     #finding what number (in the Censor_Number) corresponds to the censor type
     #values listed in cases$Censor_Number[] are factors
@@ -170,152 +168,110 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
       interval<-unname(cbind(interval,counti))
     } else {interval=NULL}
     
+    # testing to see if intervals are closed..
+    if (length(interval)!=0){
+      intervalclosed<-as.vector(interval[,1:2]) 
+      if (any(duplicated(intervalclosed))==TRUE) stop (paste(intervalclosed[which(duplicated(intervalclosed)==TRUE)],
+                                                             'is repeated in different - or I categories and this is not allowed (i.e. need closed intervals)'))
+    }
     
+
     #return values for later use
     final<-list(leftcensored1=lower1,leftcensored2=lower2, nocensored=exact,
                 rightcensored1=greater1,
-                rightcensored2=greater2, intervalcensored=interval,
-                upperbound=upperbound,
-                quasipoisson_phi = quasipoisson_phi)
-    
-    # report error.. 
-    if(length(final$leftcensored2)!=0) {leftcensored2value<-final$leftcensored2[1,1]} else {leftcensored2value=-1}
-    if(length(final$intervalcensored)!=0) {intervalvalues<-final$intervalcensored[,c(1,2)]} else {intervalvalues=-2}
-    if(length(final$rightcensored2)!=0) {rightcensored2value<-final$rightcensored2[1,1]} else {rightcensored2value=-3}
-    if(length(final$nocensored)!=0) {nocensored<-final$nocensored[1,]} else {nocensored = -4}
-    
-    # add error if user does not do interval censoring correctly 
-    if(any(intervalvalues==leftcensored2value) || 
-       any(intervalvalues==rightcensored2value) ||
-       any(intervalvalues==nocensored)) stop ('Check interval censoring.')
-    
+                rightcensored2=greater2, intervalcensored=interval
+                # lowerbound = lowerbound,
+                # upperbound=upperbound
+                )
     
     #replacing any negative numbers with 0
     #there might be negative numbers, but these negative values will cause error in the likelihood function
     final<-rapply(final,function(x) ifelse(x<0,0,x), how = "replace")
+    
+    # add errors
+    if(length(final$leftcensored2)!= 0 && length(final$intervalcensored) != 0 &&
+       final$leftcensored2[1,1] == final$intervalcensored[1,1]) stop ('Censored table can not have the same number in both the <= or LE category and the - or I category (i.e. need closed intervals)')
+    
+    if(length(final$rightcensored2)!= 0 && length(final$intervalcensored) != 0 &&
+       final$rightcensored2[1,1] == final$intervalcensored[nrow(final$intervalcensored),2]) stop ('Censored table can not have the same number in both the >= or GE or + category and the - or I category (i.e. need closed intervals)')
+    
     return(final)
   } #end fixdata_univariatecase function
   
-  #####
-  #####
-  
-  dqpois = function(n, lambda, quasipoisson_phi) {
-    mu = lambda
-    k = mu/(quasipoisson_phi - 1)
-    r = dnbinom(n, mu = mu, size = k)
-    return(r)
-  }
-  
-  pqpois = function(n, lambda, quasipoisson_phi, lower.tail) {
-    mu = lambda
-    k = mu/(quasipoisson_phi - 1)
-    r = pnbinom(n, mu = mu, size = k, lower.tail = lower.tail)
-    return(r)
-  }
-  
-  
-  #A dpois_trunc gives right-truncation Poisson probabilities with quasipoisson_phi value
-  dqpois_trunc<-function(n,lambda,quasipoisson_phi, upperbound){
-    density.output = dqpois(n,lambda, quasipoisson_phi)*(1/(1-(pqpois((upperbound-1),lambda,quasipoisson_phi, lower.tail=FALSE))))
-    return(density.output)} #end dpois_trunc_quasi
-  
-  #"ppois_truc" is a right truncated Poisson distribution function
-  #This will be used in loglikelihood_univariatecase function
-  ppois_trunc_quasi<-function(n,lambda,quasipoisson_phi, upperbound,lower.tail){
-    if (lower.tail==FALSE) {
-      x_to_up<-data.frame((n+1):upperbound) #greater than (>). NOT greater than AND equal to (>= or +)
-      dtsum=NULL
-      for (i in 1:nrow(x_to_up)){
-        dtsum[i]=dqpois_trunc(x_to_up[i,],lambda,quasipoisson_phi, upperbound)}
-      return(sum(dtsum))
-    }
-    
-    if (lower.tail==TRUE) {
-      zero_to_n<-data.frame(0:n) #less than AND equal to (<=). NOT less than (<)
-      dtsum=NULL
-      for (i in 1:nrow(zero_to_n)){
-        dtsum[i]=dqpois_trunc(zero_to_n[i,],lambda,quasipoisson_phi, upperbound)}
-      return(sum(dtsum))
-    }
-    
-  } #end ppois_truc_quasi function
-  
+
   ##### end function that help format data first ###################
   
   
   #The "loglikelihood_univariatecase" gives the Log Likelihood for a simple frequency table
-  #This function needed to be provided first in order to find the lambda that maximizes the univariate table
-  #The censoredtable provided in this function will be from the fixdata_univariatecase function
-  #This function will be ran on both hhs and soh
 
-  loglikelihood_univariatecase<-function (censoredtable,lam){
+  loglikelihood_univariatecase<-function (fixdata_univariatecase_output,par = c(mu, r)){
 
     #the if statement checks to see if the particular case even exist
     #if a cased doesn't exist then that final value for that case equals 0
-    #these if statements take the output from the fixdata_univariatecase function and reads in the category number into the ppois/dpois functions
-    #then the function multiplies the ppois/dpois output by the corresponding count number
+    #these if statements take the output from the fixdata_univariatecase function
     #the sum is taken because there could be more than 1 of a particular category (usually more multiple cases of interval and exact)
     #notice Log is taken after the ppois is computed (log.p=TRUE will NOT be the same mathematical value)
-    #also sometimes the ppois_trunc function produces -INF, this function replaces that with a 0
+    #also sometimes the trunc function produces -INF, this function replaces that with a 0
 
-    # add quasipoisson_phi value 
-    quasipoisson_phi<-censoredtable$quasipoisson_phi
+    # maximize two paramters 
+    mu = par[1]
+    r= par[2]
+    # reparameterize 
+    p = r/(r+mu)
     
     #< censor case
-    if (length(censoredtable$leftcensored1)==0) {finall1=0} else {
-      finall1=NULL
-      for (i in 1:nrow(censoredtable$leftcensored1)){
-        #have a -1 because how the ppois_trunc_quasi function works
-        #calculates less than AND equal to (<=) NOT less than (<)
-        finall1[i]=sum(log(ppois_trunc_quasi(censoredtable$leftcensored1[i,1]-1,lam,quasipoisson_phi, censoredtable$upperbound,lower.tail=TRUE))*censoredtable$leftcensored1[i,2])}
+    if (length(fixdata_univariatecase_output$leftcensored1)==0) {finall1=0} else {
+          # minus one because it is not equal to that left censored number 
+        finall1=log(pnbinom((fixdata_univariatecase_output$leftcensored1[1,1]-1), size = r, mu = mu)) *
+                    fixdata_univariatecase_output$leftcensored1[1,2]
         finall1<-replace(finall1, is.infinite(finall1),0)
       finall1=sum(finall1)}
 
     #<= censor case
-    if (length(censoredtable$leftcensored2)==0) {finall2=0} else {
-      finall2=NULL
-      for (i in 1:nrow(censoredtable$leftcensored2)){
-        finall2[i]=sum(log(ppois_trunc_quasi(censoredtable$leftcensored2[i,1],lam,quasipoisson_phi, censoredtable$upperbound,lower.tail=TRUE))*censoredtable$leftcensored2[i,2])}
+    if (length(fixdata_univariatecase_output$leftcensored2)==0) {finall2=0} else {
+      finall2=log(pnbinom(fixdata_univariatecase_output$leftcensored2[1,1],size = r, mu = mu))*
+                  fixdata_univariatecase_output$leftcensored2[1,2]
       finall2<-replace(finall2, is.infinite(finall2),0)
       finall2=sum(finall2)}
 
     #0 censor case
     #different format in for loop
     #here using [1,i], [2,i], and ncol NOT [i,1], [i,2], and nrow because how fixdata_univariatecase outputs this case
-    if (length(censoredtable$nocensored)==0) {finale=0} else {
+    if (length(fixdata_univariatecase_output$nocensored)==0) {finale=0} else {
       finale=NULL
-      for (i in 1:ncol(censoredtable$nocensored)){
-        finale[i]=sum(log(dqpois_trunc(as.numeric(censoredtable$nocensored[1,i]),lam,quasipoisson_phi, censoredtable$upperbound))*as.numeric(censoredtable$nocensored[2, i]))}
+      for (i in 1:ncol(fixdata_univariatecase_output$nocensored)){
+        finale[i]=log(dnbinom(as.numeric(fixdata_univariatecase_output$nocensored[1,i]),size = r, mu = mu))*
+                  as.numeric(fixdata_univariatecase_output$nocensored[2, i])}
       finale<-replace(finale, is.infinite(finale),0)
       finale=sum(finale)}
 
     #> censor case
-    if (length(censoredtable$rightcensored1)==0) {finalg1=0} else {
-      finalg1=NULL
-      for (i in 1:nrow(censoredtable$rightcensored1)){
-        finalg1[i]=sum(log(ppois_trunc_quasi(censoredtable$rightcensored1[i,1],lam,quasipoisson_phi, censoredtable$upperbound,lower.tail=FALSE))*censoredtable$rightcensored1[i,2])}
+    if (length(fixdata_univariatecase_output$rightcensored1)==0) {finalg1=0} else {
+    # plus one because it is not equal to that right censored number 
+      finalg1=log(pnbinom(fixdata_univariatecase_output$rightcensored1[1,1]+1,size = r, mu = mu, lower.tail=FALSE))*
+              fixdata_univariatecase_output$rightcensored1[1,2]
       finalg1<-replace(finalg1, is.infinite(finalg1),0)
       finalg1=sum(finalg1)}
 
     #>= or + censor case
-    if (length(censoredtable$rightcensored2)==0) {finalg2=0} else {
+    if (length(fixdata_univariatecase_output$rightcensored2)==0) {finalg2=0} else {
       finalg2=NULL
-      for (i in 1:nrow(censoredtable$rightcensored2)){
-        #have a -1 because how the ppois_trunc_quasi function works
-        #greater than (>) NOT greater than AND equal to(>= or +)
-        finalg2[i]=sum(log(ppois_trunc_quasi(censoredtable$rightcensored2[i,1]-1,lam,quasipoisson_phi,censoredtable$upperbound,lower.tail=FALSE))*censoredtable$rightcensored2[i,2])}
+      finalg2=log(pnbinom(fixdata_univariatecase_output$rightcensored2[1,1],size = r,mu = mu, lower.tail=FALSE))*
+              fixdata_univariatecase_output$rightcensored2[1,2]
       finalg2<-replace(finalg2, is.infinite(finalg2),0)
       finalg2=sum(finalg2)}
 
     # - censor case
-    if (sum(censoredtable$intervalcensored)==0) {finali=0} else {
+    if (sum(fixdata_univariatecase_output$intervalcensored)==0) {finali=0} else {
       finali=NULL
-      for (i in 1:nrow(censoredtable$intervalcensored)){
-        #have a -1 because how the ppois_trunc_quasi function works
-        finali[i]=sum(log(ppois_trunc_quasi(censoredtable$intervalcensored[i,2],lam,quasipoisson_phi,censoredtable$upperbound,lower.tail=TRUE)-ppois_trunc_quasi((censoredtable$intervalcensored[i,1])-1,lam,quasipoisson_phi, censoredtable$upperbound,lower.tail=TRUE))*censoredtable$intervalcensored[i,3])}
-        finali<-replace(finali, is.infinite(finali),0)
+      for (i in 1:nrow(fixdata_univariatecase_output$intervalcensored)){
+        #have a -1 for second part of interval  
+        finali[i]=log(pnbinom(fixdata_univariatecase_output$intervalcensored[i,2],size =r, mu = mu)-
+                          pnbinom(((fixdata_univariatecase_output$intervalcensored[i,1])-1),size =r, mu = mu))*
+                          fixdata_univariatecase_output$intervalcensored[i,3]}
+      finali<-replace(finali, is.infinite(finali),0)
       finali=sum(finali)}
-
+    
     #sums all the functions' final result
     #multiplied by -1 because the optim function (used below) minimizes so this is easiest way to minimize
     final<-(finall1+finall2+finale+finalg1+finalg2+finali)*-1
@@ -325,23 +281,16 @@ estimated_lambda<-function (censoredtable, upperbound, quasipoisson_phi){
   #####
   #####
 
-  #The "findlambda_univariatecase" function finds the lambda that mimizes the loglikelihood_univariate function
-  #Note that par is the intial guess, now it is random between 0 and hhs/soh upper bound
-  #findlambda_univariatecase has to be done for hhs and area (soh) seperately
-  #Remember this function will produce a right shifted lambda because the input 'censoredtable' will be the output from fixdata_univariatecase function 
-
-  findlambda_univariatecase<- function(censoredtable){
-    op<- optim(par=runif(1,0,censoredtable$upperbound),loglikelihood_univariatecase, censoredtable=censoredtable, method="Brent", lower=1, upper=censoredtable$upperbound )
-    options( warn = -1 )
-    return(op$par)
-  } #end of findlambda_univariatecase function
-
-  #####
-  #####
-  
-  #start by categorizing the censoring
-  newtabledata<-fixdata_univariatecase(censoredtable,upperbound, quasipoisson_phi)
-  #now minimize the loglikelihood_univariatecase function to find lambda from censored table
-  final=findlambda_univariatecase(newtabledata)
-  return(final)
+  # optimize mu and r in loglikelihood_univariatecase
+  # hide warnings... 
+  options(warn=-1)
+  fixdata_univariatecase_output=fixdata_univariatecase(censoredtable)
+  op<- optim(par=c(100,100),
+             fn =loglikelihood_univariatecase, 
+             fixdata_univariatecase_output=fixdata_univariatecase_output)
+  final<-op$par
+  mu = final[1]
+  r = final[2]
+  return(list("Average"= mu, "Dispersion"=r))
 }
+
